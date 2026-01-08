f6be833c8c47a0891912e7cf28bc2b2a82528274
markocevrljakovic <marecevrljakovic@gmail.com>
2026-01-08 18:31:14 +0100

    refactor: made email task executor configurable via YAML

--- backend/src/main/java/dev/marko/EmailSender/config/AsyncConfig.java
+++ backend/src/main/java/dev/marko/EmailSender/config/AsyncConfig.java
@@ -1,5 +1,8 @@
 package dev.marko.EmailSender.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -7,18 +10,26 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Executor;
 
 @Configuration
+@ConfigurationProperties(prefix = "app.async")
+@Getter
+@Setter
 public class AsyncConfig {
+
+    private int corePoolSize;
+    private int maxPoolSize;
+    private int queueCapacity;
+    private String threadNamePrefix;
 
     @Bean(name = "emailTaskExecutor")
     public Executor emailTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         // threads always active
-        executor.setCorePoolSize(10);
+        executor.setCorePoolSize(corePoolSize);
         // max pool size
-        executor.setMaxPoolSize(20);
+        executor.setMaxPoolSize(maxPoolSize);
         // queue capacity
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("EmailSender-");
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix(threadNamePrefix);
         executor.initialize();
         return executor;
     }
